e62017024cc8da48476d4a8e33efd27c83d7bfd8
Felipe <pipeba1408@gmail.com>
2025-06-02 18:33:40 -0500

    Rename class

--- src/main/java/com/felipe/airportapi/AirportApiApplication.java
+++ src/main/java/com/felipe/airportapi/AirportApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AirportapiApplication {
+public class AirportApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AirportapiApplication.class, args);
+		SpringApplication.run(AirportApiApplication.class, args);
 	}
 
 }
