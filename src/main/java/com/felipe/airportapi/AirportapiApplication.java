ffeb45601896d7034ec63e08af990a086b2ad56c
Felipe <pipeba1408@gmail.com>
2025-06-02 15:41:21 -0500

    Upload project files

---
+++ src/main/java/com/felipe/airportapi/AirportapiApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.airportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/airportapi/AirportapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.airportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
