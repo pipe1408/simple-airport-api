b659d789ae286b7ef0df907677fd411b695241a5
Felipe <pipeba1408@gmail.com>
2025-06-07 00:20:56 -0500

    Remove wrong annotation

--- src/main/java/com/felipe/airportapi/entity/dto/AirportDTO.java
+++ src/main/java/com/felipe/airportapi/entity/dto/AirportDTO.java
@@ -1,10 +1,9 @@
 package com.felipe.airportapi.entity.dto;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 
 public record AirportDTO(
         @NotNull String code,
         @NotNull String name,
-        @NotNull @Positive String cityName
+        @NotNull String cityName
 ) { }
